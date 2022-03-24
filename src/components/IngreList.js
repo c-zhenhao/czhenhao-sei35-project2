24b600abb199d58c8782a73c5c5b3adf651d228a
czhenhao <czhenhao@hotmail.com>
2022-03-25 07:40:30 +0800

    css-ed the cart

---
+++ src/components/IngreList.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import IngreModal from "./CartModal";
+
+function IngreList({ ingre }) {
+  console.log(ingre);
+  const [ingreModalState, setIngreModalState] = useState(false);
+
+  // handle modal state on click set to true
+  function handleClick() {
+    console.log("div is clicked");
+    setIngreModalState(true);
+  }
+
+  // handle modal state to false
+  function handleModalClose(event) {
+    console.log("close the modal");
+    setIngreModalState(false);
+  }
+
+  return (
+    <>
+      {/* <div className="drinksModal">
+        {ingreModalState && (
+          <IngreModal
+            ingre={ingre}
+            exitModal={handleModalClose}
+          />
+        )}
+      </div> */}
+      {/* <div className="searchCard"> */}
+      <img
+        style={{
+          width: "250px",
+          borderTopLeftRadius: "20px",
+          borderTopRightRadius: "20px",
+          backgroundColor: "#ffffff",
+          margin: "1px",
+          border: "1px transparent rgba(255,0,0,1)",
+        }}
+        src={`https://www.thecocktaildb.com/images/ingredients/${ingre}.png`}
+        alt={ingre}
+        onClick={handleClick}
+      ></img>
+      <div
+        className="searchCardTitle"
+        style={{
+          height: "50px",
+          borderBottomRightRadius: "10px",
+          borderBottomLeftRadius: "10px",
+          backgroundColor: "#4361ee",
+          marginLeft: "1px",
+          marginTop: "220px",
+          textOverflow: "none",
+          alignItems: "center",
+          fontSize: "1.1rem",
+        }}
+      >
+        <br />
+        {ingre}
+      </div>
+      {/* </div> */}
+    </>
+  );
+}
+
+export default IngreList;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,16 +1,72 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import CartResults from "../components/CartResults";
+import IngreList from "../components/IngreList";
+
+import styles from "../components/CartResults.module.css";
 
 function Cart() {
   const { cartList } = useContext(GlobalContext);
   console.log(cartList);
   console.log(typeof cartList);
 
+  // CREATE INGREDIENTS ARRAY
+  const ingre1 = cartList.map(({ ingredient1 }) => ingredient1);
+  console.log(ingre1);
+
+  const ingre2 = cartList.map(({ ingredient2 }) => ingredient2);
+  console.log(ingre2);
+
+  const ingre3 = cartList.map(({ ingredient3 }) => ingredient3);
+  console.log(ingre3);
+
+  const ingre4 = cartList.map(({ ingredient4 }) => ingredient4);
+  console.log(ingre4);
+
+  const ingre5 = cartList.map(({ ingredient5 }) => ingredient5);
+  console.log(ingre5);
+
+  const ingre6 = cartList.map(({ ingredient6 }) => ingredient6);
+  console.log(ingre6);
+
+  const concatIngre = ingre1.concat(ingre2, ingre3, ingre4, ingre5, ingre6);
+  console.log(concatIngre);
+
+  const uniqueIngre = [...new Set(concatIngre)];
+  console.log(uniqueIngre);
+
+  const cleanIngre = uniqueIngre.filter((element) => {
+    return element !== null;
+  });
+
+  console.table(cleanIngre);
+
   return (
     <>
+      <div className={styles.title}>
+        <h1>What you need to make your dreams come true</h1>
+      </div>
+
+      <div className={styles.ingreContainer}>
+        <div className={styles.ingreCarousel}>
+          {cleanIngre.length > 0 ? (
+            cleanIngre.map((ingre) => {
+              return (
+                <div className={styles.item}>
+                  <IngreList ingre={ingre} key={Math.random()} />
+                </div>
+              );
+            })
+          ) : (
+            <div className="ingreList_empty">
+              no ingredients... add some drinks!
+            </div>
+          )}
+        </div>
+      </div>
+
       <div className="title">
-        <h1>Your list</h1>
+        <h1>Your Cart</h1>
       </div>
 
       <div className="searchContainer">
